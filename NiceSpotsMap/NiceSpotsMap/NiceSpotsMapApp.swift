cd69c197c5550b49a0cf734afcb54ce024595c89
artem.denisov <artem.denisov@devexpress.com>
2022-06-14 22:47:03 +0400

    create project

---
+++ NiceSpotsMap/NiceSpotsMap/NiceSpotsMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  NiceSpotsMapApp.swift
+//  NiceSpotsMap
+//
+//  Created by Артем Денисов on 14.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct NiceSpotsMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
