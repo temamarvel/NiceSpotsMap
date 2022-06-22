1ccbdba9f8f97140f2ba99d129ca603cfba7f98d
temamarvel <tema.denisoff@gmail.com>
2022-06-22 23:15:35 +0400

    add annotations from json local file to the map

---
+++ NiceSpotsMap/NiceSpotsMap/Location.swift
@@ -0,0 +1,28 @@
+//
+//  Locatioons.swift
+//  NiceSpotsMap
+//
+//  Created by Артем Денисов on 22.06.2022.
+//
+
+import Foundation
+import CoreLocation
+
+struct Location: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    var description: String
+    var category: String
+    var city: String
+    
+    private var coordinates: Coordinates
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+    
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: coordinates.latitude, longitude: coordinates.longitude)
+    }
+}
--- NiceSpotsMap/NiceSpotsMap/MapView.swift
+++ NiceSpotsMap/NiceSpotsMap/MapView.swift
@@ -18,7 +18,7 @@ struct MapView: View {
     
     var body: some View {
         ZStack(alignment: .bottom){
-            Map(coordinateRegion: $mapViewModel.region, showsUserLocation: true)
+            Map(coordinateRegion: $mapViewModel.region, showsUserLocation: true, annotationItems:  locations){MapMarker(coordinate: $0.locationCoordinate)}
                 .accentColor(Color(.systemBlue))
                 .onAppear{ mapViewModel.checkIfLocationServicesIsEnabled() }
             
