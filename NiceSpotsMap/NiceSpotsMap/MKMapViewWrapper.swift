b9670e9a2a0e3ffd10d1184ca22c9e08d4936744
temamarvel <tema.denisoff@gmail.com>
2022-06-29 17:35:28 +0400

    add pointsOfInterestFilter support to the wrapper

--- NiceSpotsMap/NiceSpotsMap/MKMapViewWrapper.swift
+++ NiceSpotsMap/NiceSpotsMap/MKMapViewWrapper.swift
@@ -12,13 +12,22 @@ struct MKMapViewWrapper : UIViewRepresentable{
     typealias UIViewType = MKMapView
     
     @Binding var region: MKCoordinateRegion
+    @Binding var pointOfInterestFilter: MKPointOfInterestFilter
     var showsUserLocation: Bool
     var showsScale: Bool
+    
+    init(region: Binding<MKCoordinateRegion>, showsUserLocation: Bool = true, showsScale: Bool = false, pointOfInterestFilter: Binding<MKPointOfInterestFilter> = .constant(.excludingAll)) {
+        self._region = region
+        self._pointOfInterestFilter = pointOfInterestFilter
+        self.showsUserLocation = showsUserLocation
+        self.showsScale = showsScale
+    }
     
     func makeUIView(context: UIViewRepresentableContext<MKMapViewWrapper>) -> MKMapView {
         let mapView = MKMapView()
         mapView.showsUserLocation = showsUserLocation
         mapView.showsScale = showsScale
+        mapView.pointOfInterestFilter = pointOfInterestFilter
         return mapView
     }
     
--- NiceSpotsMap/NiceSpotsMap/MapView.swift
+++ NiceSpotsMap/NiceSpotsMap/MapView.swift
@@ -18,7 +18,7 @@ struct MapView: View {
     
     var body: some View {
         ZStack(alignment: .bottom){
-            MKMapViewWrapper(region: $mapViewModel.region, showsUserLocation: true, showsScale: true)
+            MKMapViewWrapper(region: $mapViewModel.region)
             //Map(coordinateRegion: $mapViewModel.region, showsUserLocation: true, annotationItems:  locations){MapMarker(coordinate: $0.locationCoordinate)}
                 .accentColor(Color(.systemBlue))
                 .onAppear{ mapViewModel.checkIfLocationServicesIsEnabled() }
