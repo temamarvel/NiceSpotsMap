1a0af077f508277866d8024d62c77486a5ca62fe
temamarvel <tema.denisoff@gmail.com>
2022-07-03 15:55:05 +0400

    add content and draggesture

--- NiceSpotsMap/NiceSpotsMap/BottomSheet.swift
+++ NiceSpotsMap/NiceSpotsMap/BottomSheet.swift
@@ -7,22 +7,37 @@
 
 import SwiftUI
 
-struct BottomSheet: View {
+struct BottomSheet<Content>: View where Content: View {
+    @GestureState private var gestureState: CGFloat = 0
+    @State private var offset: CGFloat = 0
+    let content: Content
+    
+    init(@ViewBuilder content: () -> Content) {
+        self.content = content()
+    }
+    
     var body: some View {
         GeometryReader{ geomerty in
             VStack{
-                //Text("Test text")
+                self.content
             }
             .frame(width: geomerty.size.width, height: geomerty.size.height)
             .background(Color(.secondarySystemBackground))
             .cornerRadius(50)
-            .offset(y: 30)
+            .offset(y: self.offset + self.gestureState)
+            .animation(.interactiveSpring(), value: self.offset)
+            .gesture(DragGesture()
+                .updating($gestureState){ currentState, gestureState, transaction in gestureState = currentState.translation.height }
+                .onEnded{ value in self.offset = value.location.y }
+            )
         }
     }
 }
-    
-    struct BottomSheet_Previews: PreviewProvider {
-        static var previews: some View {
-            BottomSheet()
+
+struct BottomSheet_Previews: PreviewProvider {
+    static var previews: some View {
+        BottomSheet(){
+            Text("Test text")
         }
     }
+}
